e599aa3e22dbe0815e2703f97f8bfe99ebeaaa19
Katy DeCorah <katydecorah@gmail.com>
2020-12-13 09:56:10 -0500

    Reorganize (#3)
    * rm node_modules
    
    * Use ncc
    
    * Update deps
    
    * Clean up deps
    
    * Update main.yml
    
    * 🎵 2020 Fall
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ src/action.js
@@ -0,0 +1,13 @@
+const script = require("./index.js");
+const core = require("@actions/core");
+
+try {
+  script.playlist({}, null, (err, callback) => {
+    if (err) {
+      core.setFailed(err.message);
+    }
+    console.log(callback);
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
