6772cc123f4505df10667f04d12b210fd24e0cc2
Katy DeCorah <katydecorah@gmail.com>
2022-01-20 09:51:08 -0500

    write file tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,12 +1,11 @@
 import { action } from "..";
-import updateMain from "spotify-to-jekyll/src/update-main.js";
+import * as UpdateMain from "spotify-to-jekyll/src/update-main.js";
 import { exportVariable } from "@actions/core";
 import * as ListPlaylists from "../list-playlists";
 import * as LearnPlaylistName from "../learn-playlist-name";
+import { promises } from "fs";
 
 jest.mock("@actions/core");
-jest.mock("spotify-to-jekyll/src/update-main.js");
-jest.mock("fs");
 jest.mock("spotify-web-api-node", () => {
   return jest.fn().mockImplementation(() => {
     const userPlaylists = require("./fixtures/get-user-playlists.json");
@@ -36,6 +35,12 @@ describe("action", () => {
     process.env.YEAR = "2021";
     const learnPlaylistNameSpy = jest.spyOn(LearnPlaylistName, "default");
     const listPlaylistsSpy = jest.spyOn(ListPlaylists, "default");
+    const updateMainSpy = jest.spyOn(UpdateMain, "default");
+    jest.spyOn(promises, "readFile").mockResolvedValue(`
+- playlist: 2021 Summer
+`);
+    const writeFileSpy = jest.spyOn(promises, "writeFile").mockImplementation();
+
     await action();
     expect(learnPlaylistNameSpy).toHaveReturnedWith("2021 Fall");
     const results = listPlaylistsSpy.mock.results[0].value;
@@ -51,111 +56,73 @@ describe("action", () => {
       "PlaylistImage",
       "https://mosaic.scdn.co/640/ab67616d0000b27304a205b54b5c1fbae8543efaab67616d0000b27364fa86e376b0279aa49a1832ab67616d0000b273acddc5a09b8cb4730995b60aab67616d0000b273ba738766498bddccae3d319a"
     );
-    expect(updateMain.mock.calls[0]).toMatchInlineSnapshot(`
+    expect(updateMainSpy.mock.calls[0]).toMatchSnapshot();
+    expect(writeFileSpy.mock.calls[0]).toMatchInlineSnapshot(`
       Array [
-        Object {
-          "formatted_name": "2021-fall",
-          "image": "2021-fall.png",
-          "name": "2021 Fall",
-          "tracks": Array [
-            Object {
-              "album": "one hand on the steering wheel the other sewing a garden",
-              "artist": "Ada Lea",
-              "name": "can't stop me from dying",
-            },
-            Object {
-              "album": "No Shadow",
-              "artist": "Hyd",
-              "name": "No Shadow",
-            },
-            Object {
-              "album": "Trip To Japan",
-              "artist": "The Shacks",
-              "name": "Trip To Japan",
-            },
-            Object {
-              "album": "Keeper",
-              "artist": "Hana Vu",
-              "name": "Maker",
-            },
-            Object {
-              "album": "Doomin' Sun",
-              "artist": "Bachelor, Jay Som, Palehound",
-              "name": "Anything at All",
-            },
-            Object {
-              "album": "Would You Mind Please Pulling Me Close?",
-              "artist": "Tasha",
-              "name": "Would You Mind Please Pulling Me Close?",
-            },
-            Object {
-              "album": "Genesis",
-              "artist": "Spencer.",
-              "name": "Genesis",
-            },
-            Object {
-              "album": "Old Peel",
-              "artist": "Aldous Harding",
-              "name": "Old Peel",
-            },
-            Object {
-              "album": "Everybody's Birthday",
-              "artist": "Hana Vu",
-              "name": "Everybody's Birthday",
-            },
-            Object {
-              "album": "The Baby",
-              "artist": "Samia",
-              "name": "Big Wheel",
-            },
-            Object {
-              "album": "-io",
-              "artist": "Circuit des Yeux",
-              "name": "Dogma",
-            },
-            Object {
-              "album": "private LIFE",
-              "artist": "Virginia Wing",
-              "name": "I'm Holding Out For Something",
-            },
-            Object {
-              "album": "Blue Weekend",
-              "artist": "Wolf Alice",
-              "name": "Delicious Things",
-            },
-            Object {
-              "album": "Bottle Episode",
-              "artist": "Mandy, Indiana",
-              "name": "Bottle Episode",
-            },
-            Object {
-              "album": "The Gaping Mouth",
-              "artist": "Lowertown",
-              "name": "The Gaping Mouth",
-            },
-            Object {
-              "album": "Fantasize Your Ghost",
-              "artist": "Ohmme",
-              "name": "3 2 4 3",
-            },
-            Object {
-              "album": "You Think It's Like This But Really It's Like This",
-              "artist": "Mirah",
-              "name": "Of Pressure",
-            },
-            Object {
-              "album": "When the Sun Comes Up",
-              "artist": "Greta Morgan",
-              "name": "When the Sun Comes Up",
-            },
-            Object {
-              "album": "Ceremony",
-              "artist": "Anna von Hausswolff",
-              "name": "Mountains Crave",
-            },
-          ],
-          "url": "https://open.spotify.com/playlist/2YnPs9UNBkJpswmsRNwQ1o",
-        },
+        "_data/playlists.yml",
+        "
+      - playlist: 2021 Summer
+      - playlist: 2021 Fall
+        spotify: https://open.spotify.com/playlist/2YnPs9UNBkJpswmsRNwQ1o
+        tracks:
+        - track: \\"can't stop me from dying\\"
+          artist: \\"Ada Lea\\"
+          album: \\"one hand on the steering wheel the other sewing a garden\\"
+        - track: \\"No Shadow\\"
+          artist: \\"Hyd\\"
+          album: \\"No Shadow\\"
+        - track: \\"Trip To Japan\\"
+          artist: \\"The Shacks\\"
+          album: \\"Trip To Japan\\"
+        - track: \\"Maker\\"
+          artist: \\"Hana Vu\\"
+          album: \\"Keeper\\"
+        - track: \\"Anything at All\\"
+          artist: \\"Bachelor, Jay Som, Palehound\\"
+          album: \\"Doomin' Sun\\"
+        - track: \\"Would You Mind Please Pulling Me Close?\\"
+          artist: \\"Tasha\\"
+          album: \\"Would You Mind Please Pulling Me Close?\\"
+        - track: \\"Genesis\\"
+          artist: \\"Spencer.\\"
+          album: \\"Genesis\\"
+        - track: \\"Old Peel\\"
+          artist: \\"Aldous Harding\\"
+          album: \\"Old Peel\\"
+        - track: \\"Everybody's Birthday\\"
+          artist: \\"Hana Vu\\"
+          album: \\"Everybody's Birthday\\"
+        - track: \\"Big Wheel\\"
+          artist: \\"Samia\\"
+          album: \\"The Baby\\"
+        - track: \\"Dogma\\"
+          artist: \\"Circuit des Yeux\\"
+          album: \\"-io\\"
+        - track: \\"I'm Holding Out For Something\\"
+          artist: \\"Virginia Wing\\"
+          album: \\"private LIFE\\"
+        - track: \\"Delicious Things\\"
+          artist: \\"Wolf Alice\\"
+          album: \\"Blue Weekend\\"
+        - track: \\"Bottle Episode\\"
+          artist: \\"Mandy, Indiana\\"
+          album: \\"Bottle Episode\\"
+        - track: \\"The Gaping Mouth\\"
+          artist: \\"Lowertown\\"
+          album: \\"The Gaping Mouth\\"
+        - track: \\"3 2 4 3\\"
+          artist: \\"Ohmme\\"
+          album: \\"Fantasize Your Ghost\\"
+        - track: \\"Of Pressure\\"
+          artist: \\"Mirah\\"
+          album: \\"You Think It's Like This But Really It's Like This\\"
+        - track: \\"When the Sun Comes Up\\"
+          artist: \\"Greta Morgan\\"
+          album: \\"When the Sun Comes Up\\"
+        - track: \\"Mountains Crave\\"
+          artist: \\"Anna von Hausswolff\\"
+          album: \\"Ceremony\\"
+      ",
       ]
     `);
   });
