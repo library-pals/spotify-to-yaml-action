45749ae9c9843d5b0fc4e5e5353c8de8c5744792
Katy DeCorah <katydecorah@gmail.com>
2022-01-20 12:57:33 -0500

    Remove spotify-to-jekyll

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import updateMain from "spotify-to-jekyll/src/update-main.js";
-import { setFailed, exportVariable } from "@actions/core";
+import updateMain from "./write-file";
+import { setFailed, exportVariable, getInput } from "@actions/core";
 import learnPlaylistName from "./learn-playlist-name";
 import listPlaylists from "./list-playlists";
 
@@ -17,17 +17,16 @@ export type Playlist = {
 
 export async function action() {
   try {
+    const fileName = getInput("fileName");
     const playlistName: string = learnPlaylistName();
     const playlist = (await listPlaylists(playlistName)) as Playlist;
     // export image variable to be downloaded latter
-    if (playlist.image) {
-      exportVariable("PlaylistImageOutput", `${playlist.formatted_name}.png`);
-      exportVariable("PlaylistImage", playlist.image);
-      // replace Spotify image url with local version
-      playlist.image = `${playlist.formatted_name}.png`;
-    }
+    exportVariable("PlaylistImageOutput", `${playlist.formatted_name}.png`);
+    exportVariable("PlaylistImage", playlist.image);
+    // replace Spotify image url with local version
+    playlist.image = `${playlist.formatted_name}.png`;
     // save tracks to playlists.yml
-    await updateMain(playlist);
+    await updateMain(playlist, fileName);
   } catch (error) {
     setFailed(error.message);
   }
--- src/list-playlists.ts
+++ src/list-playlists.ts
@@ -1,6 +1,5 @@
 import { setFailed, getInput } from "@actions/core";
 import SpotifyWebApi from "spotify-web-api-node";
-import formatTracks from "spotify-to-jekyll/src/format-tracks.js";
 import { Playlist } from "./index.js";
 
 export default async function listPlaylists(
@@ -18,7 +17,9 @@ export default async function listPlaylists(
   spotifyApi.setAccessToken(access_token);
 
   const { body } = await spotifyApi.getUserPlaylists(username);
-  const findPlaylist = body.items.find((list) => list.name === listName);
+  const findPlaylist: SpotifyPlaylist = body.items.find(
+    ({ name }) => name === listName
+  );
   if (!findPlaylist) {
     setFailed(`Could not find playlist "${listName}". Is it private?`);
     return;
@@ -30,8 +31,150 @@ export default async function listPlaylists(
     name: findPlaylist.name,
     external_urls: findPlaylist.external_urls,
     images: findPlaylist.images,
-    tracks: {
-      items,
-    },
+    tracks: items,
   });
 }
+
+export function formatTracks({
+  name,
+  external_urls,
+  images,
+  tracks,
+}: {
+  name: string;
+  external_urls: SpotifyPlaylist["external_urls"];
+  images: SpotifyPlaylist["images"];
+  tracks: SpotifyTrack[];
+}): Playlist {
+  const largestImage = images.sort((a, b) => b.width - a.width)[0];
+  return {
+    name,
+    formatted_name: name.replace("/", "-").toLowerCase().replace(" ", "-"),
+    url: external_urls.spotify,
+    tracks: tracks.map(({ track }) => ({
+      name: track.name,
+      artist: track.artists.map(({ name }) => name).join(", "),
+      album: track.album.name,
+    })),
+    image: largestImage.url,
+  };
+}
+
+export type SpotifyTrack = {
+  added_at: string;
+  added_by: {
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  is_local: boolean;
+  primary_color: string | null;
+  track: {
+    album: {
+      album_type: string;
+      artists: [
+        {
+          external_urls: {
+            spotify: string;
+          };
+          href: string;
+          id: string;
+          name: string;
+          type: string;
+          uri: string;
+        }
+      ];
+      available_markets: string[];
+      external_urls: {
+        spotify: string;
+      };
+      href: string;
+      id: string;
+      images: {
+        height: number;
+        url: string;
+        width: number;
+      }[];
+
+      name: string;
+      release_date: string;
+      release_date_precision: string;
+      total_tracks: number;
+      type: string;
+      uri: string;
+    };
+    artists: {
+      external_urls: {
+        spotify: string;
+      };
+      href: string;
+      id: string;
+      name: string;
+      type: string;
+      uri: string;
+    }[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    episode: boolean;
+    explicit: boolean;
+    external_ids: {
+      isrc: string;
+    };
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string;
+    track: true;
+    track_number: number;
+    type: string;
+    uri: string;
+  };
+  video_thumbnail: {
+    url: string | null;
+  };
+};
+
+export type SpotifyPlaylist = {
+  collaborative: boolean;
+  description: string;
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: {
+    height: number;
+    url: string;
+    width: number;
+  }[];
+  name: string;
+  owner: {
+    display_name: string;
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  primary_color: string | null;
+  public: boolean;
+  snapshot_id: string;
+  tracks: {
+    href: string;
+    total: number;
+  };
+  type: string;
+  uri: string;
+};
