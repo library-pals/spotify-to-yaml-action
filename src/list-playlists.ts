c4871cea51b7d1874b085c87a7f4d926357c3c8e
Katy DeCorah <katydecorah@gmail.com>
2024-06-05 20:51:03 -0400

    Refactor

--- src/list-playlists.ts
+++ src/list-playlists.ts
@@ -6,26 +6,11 @@ export default async function listPlaylists(
   listName: string
 ): Promise<Playlist> {
   try {
-    const spotifyApi = new SpotifyWebApi({
-      clientId: process.env.SpotifyClientID,
-      clientSecret: process.env.SpotifyClientSecret,
-    });
+    const spotifyApi = await initializeSpotifyApi();
     const username = getInput("spotify-username");
-    const {
-      body: { access_token },
-    } = await spotifyApi.clientCredentialsGrant();
 
-    spotifyApi.setAccessToken(access_token);
-
-    const { body } = await spotifyApi.getUserPlaylists(username);
-    const findPlaylist = body.items.find(({ name }) => name === listName);
-    if (!findPlaylist) {
-      throw new Error(`Could not find playlist "${listName}". Is it private?`);
-    }
-    const {
-      body: { items },
-    } = await spotifyApi.getPlaylistTracks(findPlaylist.id);
-    if (!items.length) throw new Error("Playlist has no tracks.");
+    const findPlaylist = await getPlaylist(spotifyApi, username, listName);
+    const items = await getPlaylistTracks(spotifyApi, findPlaylist.id);
 
     return formatTracks({
       name: findPlaylist.name,
@@ -36,6 +21,58 @@ export default async function listPlaylists(
   } catch (error) {
     throw new Error(error);
   }
+}
+
+async function getPlaylist(
+  spotifyApi: SpotifyWebApi,
+  username: string,
+  listName: string
+): Promise<SpotifyApi.PlaylistObjectSimplified> {
+  const { body } = await spotifyApi.getUserPlaylists(username);
+  const findPlaylist = body.items.find(({ name }) => name === listName);
+
+  if (!findPlaylist) {
+    throw new Error(`Could not find playlist "${listName}". Is it private?`);
+  }
+
+  return findPlaylist;
+}
+
+async function getPlaylistTracks(
+  spotifyApi: SpotifyWebApi,
+  playlistId: string
+): Promise<SpotifyApi.PlaylistTrackObject[]> {
+  const {
+    body: { items },
+  } = await spotifyApi.getPlaylistTracks(playlistId);
+
+  if (!items.length) throw new Error("Playlist has no tracks.");
+
+  return items;
+}
+
+export async function initializeSpotifyApi(): Promise<SpotifyWebApi> {
+  const clientId = process.env.SpotifyClientID;
+  const clientSecret = process.env.SpotifyClientSecret;
+
+  if (!clientId || !clientSecret) {
+    throw new Error(
+      "Missing SpotifyClientID or SpotifyClientSecret in environment variables"
+    );
+  }
+
+  const spotifyApi = new SpotifyWebApi({
+    clientId,
+    clientSecret,
+  });
+
+  const {
+    body: { access_token },
+  } = await spotifyApi.clientCredentialsGrant();
+
+  spotifyApi.setAccessToken(access_token);
+
+  return spotifyApi;
 }
 
 export function formatTracks({
