640330edf2d71f58d72b53f4ffe11dcda214623d
Katy DeCorah <katydecorah@gmail.com>
2024-06-05 20:58:09 -0400

    Update learn-playlist-name.ts

--- src/learn-playlist-name.ts
+++ src/learn-playlist-name.ts
@@ -2,11 +2,41 @@ import { getInput } from "@actions/core";
 import * as github from "@actions/github";
 
 export default function learnPlaylistName(): string {
-  const payload = github.context.payload.inputs;
+  // Extract the playlist name from the payload if it exists
+  const payload = github.context.payload?.inputs;
   if (payload && payload["playlist-name"]) {
     return payload["playlist-name"];
   }
 
+  // Get the current month and year, or use the provided environment variables
+  const { month, year } = getMonthYear();
+
+  // Define the end of season names
+  const [marchEnd, juneEnd, septemberEnd, decemberEnd] = validateSeasonNames();
+
+  // Map the end of season months to their names
+  const seasons: Record<number, string> = {
+    2: marchEnd,
+    5: juneEnd,
+    8: septemberEnd,
+    11: decemberEnd,
+  };
+
+  // Get the season name for the current month
+  const season = seasons[month];
+
+  // Throw an error if the current month is not an end of season month
+  if (!season) {
+    throw new Error(`The current month does not match an end of season month.`);
+  }
+
+  // Return the playlist name, which is the year and season name
+  // If the month is March, the year is the previous year and the current year
+  const playlistYear = month === 2 ? `${year - 1}/${year}` : year;
+  return `${playlistYear} ${season}`;
+}
+
+function getMonthYear(): { month: number; year: number } {
   const today = new Date();
   const month = process.env.MONTH
     ? parseInt(process.env.MONTH)
@@ -14,20 +44,14 @@ export default function learnPlaylistName(): string {
   const year = process.env.YEAR
     ? parseInt(process.env.YEAR)
     : today.getFullYear();
-  const [marchEnd, juneEnd, septemberEnd, decemberEnd] = validateSeasonNames();
-  const seasons = {
-    2: marchEnd,
-    5: juneEnd,
-    8: septemberEnd,
-    11: decemberEnd,
+
+  return {
+    month,
+    year,
   };
-  const season = seasons[month];
-  if (!season)
-    throw new Error(`The current month does not match an end of season month.`);
-  return `${month === 2 ? `${year - 1}/${year}` : year} ${season}`;
 }
 
-function validateSeasonNames() {
+function validateSeasonNames(): string[] {
   const seasonNames = getInput("season-names")
     .split(",")
     .map((s) => s.trim());
