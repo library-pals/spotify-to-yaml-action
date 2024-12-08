470043d83fd19fbd857ed5ea1bd26df4ab902362
Katy DeCorah <katydecorah@gmail.com>
2024-12-08 08:34:44 -0500

    Update dependencies (#152)
    * Update dependencies
    
    * Build action
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/__test__/list-playlists.test.ts
+++ src/__test__/list-playlists.test.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 import listPlaylists, { formatName, formatTracks } from "../list-playlists";
 import { getInput } from "@actions/core";
 import SpotifyWebApi from "spotify-web-api-node";
